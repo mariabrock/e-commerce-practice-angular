6e6dbce6b525d13bed60a064621c87514410bcf5
Maria Brock <brockm@ornl.giv>
2024-02-29 10:02:48 -0500

    OH MY GOSH IT DOES A THING

--- src/app/components/mens-page/mens-page.component.ts
+++ src/app/components/mens-page/mens-page.component.ts
@@ -1,9 +1,9 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatCard } from "@angular/material/card";
-import { NgForOf } from "@angular/common";
+import { AsyncPipe, NgForOf } from "@angular/common";
 import { RouterLink } from "@angular/router";
 import { ProductService } from "../../../shared/services/product.service";
-import { filter, map, Observable, tap } from "rxjs";
+import { map, Observable } from "rxjs";
 import { Product } from "../../../shared/product";
 
 @Component({
@@ -12,34 +12,20 @@ import { Product } from "../../../shared/product";
   imports: [
     MatCard,
     NgForOf,
-    RouterLink
+    RouterLink,
+    AsyncPipe
   ],
   templateUrl: './mens-page.component.html',
   styleUrl: './mens-page.component.scss'
 })
-export class MensPageComponent implements OnInit {
-
-  items: Product[] = [];
-
-  get products(): Observable<Product[]> {
-    return this.productService.products;
-  }
+export class MensPageComponent {
 
   public productService = inject(ProductService);
 
-  ngOnInit() {
-    this.products.pipe(
-      tap(data => console.log(data.map(res => {
-        res.type
-      }))),
-      // filter((data) => data.type === 'mens')
-    ).subscribe(res => {
-        res = this.items
-      console.log(this.items)
-
-    }
-
+  protected items: Observable<Product[]> = this.productService.products.pipe(
+    map((products: Product[]) =>
+    products.filter((p: Product) => p.type === 'mens')
     )
-  }
+  )
 
 }
--- src/app/components/womens-page/womens-page.component.ts
+++ src/app/components/womens-page/womens-page.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ProductService } from "../../../shared/services/product.service";
+import { map, Observable } from "rxjs";
+import { Product } from "../../../shared/product";
+import { AsyncPipe, NgForOf } from "@angular/common";
+import { MatCard } from "@angular/material/card";
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-womens-page',
   standalone: true,
-  imports: [],
+  imports: [
+    AsyncPipe,
+    MatCard,
+    NgForOf,
+    RouterLink
+  ],
   templateUrl: './womens-page.component.html',
   styleUrl: './womens-page.component.scss'
 })
 export class WomensPageComponent {
+
+  public productService = inject(ProductService);
+
+  protected items: Observable<Product[]> = this.productService.products.pipe(
+    map((products: Product[]) =>
+      products.filter((p: Product) => p.type === 'womens')
+    )
+  )
 
 }
